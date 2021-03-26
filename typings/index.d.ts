121ebbf4fe9c8705491913c5b10a46f65110a374
vgamez <v.gamez@englobatech.com>
2021-03-26 12:25:29 +0100

    initial

---
+++ typings/index.d.ts
@@ -0,0 +1,24 @@
+// Unlike the class component setState, the updates are not allowed to be partial
+declare type SetStateAction<S> = S | ((prevState: S) => S);
+/**
+ * Returns a stateful value, and a function to update it.
+ *
+ * @version 16.8.0
+ * @see https://reactjs.org/docs/hooks-reference.html#usestate
+ */
+declare function useUndoState<S>(
+	initialState: S | (() => S)
+): [S, Dispatch<SetStateAction<S>>];
+// convenience overload when first argument is omitted
+/**
+ * Returns a stateful value, and a function to update it.
+ *
+ * @version 16.8.0
+ * @see https://reactjs.org/docs/hooks-reference.html#usestate
+ */
+declare function useUndoState<S = undefined>(): [
+	S | undefined,
+	Dispatch<SetStateAction<S | undefined>>
+];
+
+export { useUndoState };
